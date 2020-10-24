06d17e33d54838d86049eb443b0014d71ab0f2bd
philippschmalen <philippschmalen@gmail.com>
2020-10-24 15:46:59 +0200

    added inspect_drop_rows_retain_columns()

--- src/Exploration.py
+++ src/Exploration.py
@@ -30,6 +30,11 @@ class Exploration:
 		print('*'*40)
 			
 
+
+	# 
+	# Inspect and handle missings
+	# 
+	
 	def inspect_missings(data, verbose=True):
 		"""Inspect missings across rows and across columns
 		
@@ -88,4 +93,66 @@ class Exploration:
 		
 		return pd.concat([df_colmiss_idx, df_colmiss_idx_share], axis=1, keys=['missing_count', 'missing_share'])
 
+
+	def inspect_drop_rows_retain_columns(data, max_missing=3):
+	    """Dropping rows with many missings for certain columns 
+	        to keep columns
+	    :param data: dataframe
+	    :param max_missing: defines until which column-wise missing count should be iterated
+	    :return tuple of two numpy arrays for plotting: Columns to keep vs. rows dropped (%)
+	    """
+	    # count missings per column, store in series
+	    count_column_missing = data.isna().sum(axis=0).sort_values(ascending=False)
+	    column_missing_names = count_column_missing.index
+
+	    # get array of missing count with unique values to loop over
+	    count_column_missing_unique = count_column_missing[count_column_missing >= 0].sort_values().unique()
+
+	    # define until which column-wise missing count should be iterated
+	    plot_x, plot_y = np.zeros(max_missing), np.zeros(max_missing)
+
+	    print("Trade off rows against columns\nDrop rows and modifiy original dataframe to keep more columns\n")
+	    print("Keep Columns\tDrop Rows [%]")
+	    for arr_idx, i in enumerate(count_column_missing_unique[:max_missing]):
+	        select_columns = count_column_missing[count_column_missing <= i].index
+
+	        # return indices (=ticker) where the 1 missing per column occurs
+	        indices_many_missings = list(data.loc[data[select_columns].isnull().any(1),:].index)
+
+	        ## compare modified vs. raw dropped missings
+	        # modified df 
+	        df_fin_mod = data.drop(indices_many_missings).dropna(axis=1)
+	        df_fin_nomiss = df_fin_mod
+	        # raw df
+	        df_raw = data.dropna(axis=1)
+
+	        ### Benefits of dropping x rows
+	        ## comparison of dropping and keeping
+	        # original 
+	        n_row_orig, n_col_orig = data.shape
+	        # modified & cleaned 
+	        n_row_mod, n_col_mod = df_fin_mod.shape
+	        # raw & cleaned
+	        n_row_nomod, n_col_nomod = df_fin_raw_nomiss.shape
+
+	        ## STATISTICS
+	        # dropped rows to modify df (count and %)
+	        n_dropped_rows_mod = len(indices_many_missings)
+	        pct_dropped_rows_mod = round(n_dropped_rows_mod/n_row_orig*100)
+
+	        # dropped cols for modified df (count and %)
+	        n_dropped_cols_mod = n_col_orig - n_col_mod
+	        pct_dropped_cols_mod = round(n_dropped_cols_mod/n_col_orig*100)
+
+	        # dropped cols for non-modified df (count and %)
+	        n_dropped_cols_nomod = n_col_orig - n_col_nomod
+	        pct_dropped_cols_nomod = round(n_dropped_cols_nomod/n_col_orig*100)
+
+	        # plot dropped rows against retained columns 
+	        # (x=100-pct_dropped_rows_mod, y=pct_dropped_rows_mod)
+	        plot_x[arr_idx], plot_y[arr_idx] = 100-pct_dropped_cols_mod, pct_dropped_rows_mod
+
+	        print("{}\t\t{}".format(plot_x[arr_idx], plot_y[arr_idx])) 
+
+	    return plot_x, plot_y
 
